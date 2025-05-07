f75dfd6f2249adce66000bc8c69acf1487475940
ScarletFangs <sirkao322@gmail.com>
2025-05-07 14:52:11 -0700

    Revert "made about/account page + navbar in layout"
    This reverts commit 3f8e94efc3048067920b257460c377caa0c49d1e.

--- src/app/about/page.tsx
+++
@@ -1,6 +0,0 @@
-
-export default async function AccountLayout(){
-  return (
-    <h1>testing account</h1>
-  );
-}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -40,15 +40,15 @@ function HamurgerSqueeze() {
     // Dropdown Icon (3 bars)
     <Menu as="div" className="relative ml-3">
       <div>
-        <MenuButton className="flex flex-col justify-center items-center "
+        <MenuButton className="flex flex-col justify-center items-center"
         >
-          <span className={`bg-white block transition-all duration-300 ease-out 
+          <span className={`bg-gray-500 block transition-all duration-300 ease-out 
                           h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`} >
           </span>
-          <span className={`bg-white block transition-all duration-300 ease-out 
+          <span className={`bg-gray-500 block transition-all duration-300 ease-out 
                           h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`} >
           </span>
-          <span className={`bg-white block transition-all duration-300 ease-out 
+          <span className={`bg-gray-500 block transition-all duration-300 ease-out 
                           h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`} >
           </span>    
         </MenuButton>
