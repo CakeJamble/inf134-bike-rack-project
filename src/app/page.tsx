f458e4c95dd8723da7ec9876d1e67eb484229781
Cole O'Balle <coballe@uci.edu>
2025-06-03 14:49:40 -0700

    Marker component
    Markers do not appear yet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
 import Image from 'next/image';
 import NavBar from "@/components/navbar";
 import MyMap from "@/components/map";
+import { Button } from '@headlessui/react';
+import './page.css'
+import { AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
 
 export default function Home() {
   return (
-  <>
+    <>
       {/* <NavBar /> */} {/* Sirena commented this out 5/7 moved to outer layout.tsx*/}
-    <div className="grid grid-rows-[200fr_2px] items-center justify-items-center min-h-screen sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <MyMap />
-    </div>
-  </>
+      <div className="grid grid-rows-[200fr_2px] items-center justify-items-center min-h-screen sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <MyMap />
+        <Button id="toLocation" className="rounded px-5 py-5 text-sm text-white">
+          <Image
+            className="pfp"
+            src="/location-arrow.png"
+            alt="to my location"
+            width="25"
+            height="25"
+          />
+        </Button>
+      </div>
+    </>
   );
 }
